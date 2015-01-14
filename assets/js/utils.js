e6dc7db7a1061fd992ae1dada6e9ddbaced32992
John <johnmarksjr@gmail.com>
2015-01-13 20:35:01 -0700

    Fixed project overlay layout issues
    Got affine transforms working
    Got ng-bing-html project title working

--- assets/js/utils.js
+++
@@ -1,9 +0,0 @@
-var Utils = {}
-Utils.Constructor = function() {
-}
-Utils.Constructor.prototype.isNumeric = function(n) {
-	  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-Utils.Constructor.prototype.numberWithCommas = function(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
