e00d8334c7d0d88dfbdb4caf6bc3c02859dec328
jettagozoom <johnmarksjr@gmail.com>
2015-02-03 20:53:40 -0700

    removed closing slash on the self-closing <br> tag

--- views/work/work.js
+++ views/work/work.js
@@ -23,11 +23,11 @@ portfolioApp.controller('WorkCtrl', ['$rootScope', '$scope',
                     "only once (as a singleton), and properly injecting the component to other components as they are requested.</li>" +
                 "<li><b>Directives</b> - As someone said, \"AngularJS is HTML6\", and directives make it so. " +
                     "Simply create your own <em>tag</em>, code up the directive, and declare your tags " +
-                    "throughout the HTML.</li><br/>" +
+                    "throughout the HTML.</li><br>" +
             "</ul>" +
             "<p>Testing in Angular is a bit tricky, but because of DI, it makes creating mocks much easier. The " +
                 "Jasmine and Karma testing frameworks are introduced where controllers, services, and directives are " +
-                "brought under test. </p><br/>" +
+                "brought under test. </p><br>" +
             "<p>The final unit was dedicated to creating a capstone project. John chose to enhance his PIP MVP Snippet Tool " +
                 "project called <em>SomeCode</em>. Three months was spent working on <em>SomeCode</em> where the " +
                 "old jQuery code was re-written using the declarative nature of AngularJS. " +
@@ -37,7 +37,7 @@ portfolioApp.controller('WorkCtrl', ['$rootScope', '$scope',
             {
                 id: "somecode",
                 href: "http://somecode.pythonanywhere.com/",
-                title: "SomeCode<br/>Snippet Tool",
+                title: "SomeCode<br>Snippet Tool",
                 orientation: "wide",
                 hostenv: "Website",
                 indata: {xoff: 30, yoff: 0, zoom: 130},
@@ -154,12 +154,12 @@ portfolioApp.controller('WorkCtrl', ['$rootScope', '$scope',
             "<p>John spun up on HTML, CSS, introductory JavaScript, jQuery, Git/GitHub, AJAX, SPA, Responsive design, " +
                 "color theory, and creating wireframes and mockups. Many exercises and projects were created throughout " +
             "the class. Below are a few of John's projects, showing introductory examples up to his final <em>Stock " +
-            "Portfolio</em> mashup.</p><br/>" +
+            "Portfolio</em> mashup.</p><br>" +
             "<p>To further his abilities in JavaScript, John internalized the following books:</p>" +
             "<ul>" +
                 "<li><b>JavaSript</b>: The Good Parts by Douglas Crockford</li>" +
                 "<li><b>JavaSript</b> Patterns by Stoyan Stefanov</li>" +
-            "</ul><br/>" +
+            "</ul><br>" +
             "<p>The final project was a personal online portfolio, which you are now viewing.",
         projects: [
             {
@@ -192,7 +192,7 @@ portfolioApp.controller('WorkCtrl', ['$rootScope', '$scope',
             {
                 id: "pamsdoterra",
                 href: "http://pamsdoterra.herokuapp.com",
-                title: "Pam's doTerra<br/>Invitations",
+                title: "Pam's doTerra<br>Invitations",
                 orientation: "wide",
                 hostenv: "Website",
                 indata: {xoff: 0, yoff: 70, zoom: 110},
@@ -248,19 +248,19 @@ portfolioApp.controller('WorkCtrl', ['$rootScope', '$scope',
             "as an engineer in their computer graphics lab, he worked on many interesting and challenging projects. " +
             "In the graphics lab he worked on the new at the time X11 windowing system, writing graphics drivers, " +
             "producing the world's fastest X11 implementation, and adding cool functionality such as multi-monitor " +
-            "and multi-layer (image/overlay) support.</p><br/>" +
+            "and multi-layer (image/overlay) support.</p><br>" +
             "<p>John then became interested in software collaboration and was one of three engineers to start a group " +
             "within HP that produced " +
             "the <b>RGS</b> (Remote Graphics Software) product. " +
             "His experience in the graphics space was just what was needed for a " +
             "successful and performant product. Here his skills centered on C++, OO A&D, multi-threaded support, ICE, Qt, " +
-            "cross platform support, and organizing team dynamics using Agile (XP, Scrum) techniques.</p><br/>" +
+            "cross platform support, and organizing team dynamics using Agile (XP, Scrum) techniques.</p><br>" +
             "<p>John's final project got him involved in helping to port the Palm webOS operating system to run on " +
             "a PC desktop. " +
             "There were many challenges, very complex builds, and fun problems to solve. webOS on PC was successfully " +
             "ported but was later " +
             "mothballed when HP decided to stop investing in webOS. Shortly after the close of the project John took early " +
-            "retirement from HP, and decided to pursue other career interests.</p><br/>" +
+            "retirement from HP, and decided to pursue other career interests.</p><br>" +
             "<p>Patents Held:</p>" +
             "<ol class='patent'>" +
                 "<li><a href='http://patft1.uspto.gov/netacgi/nph-Parser?patentnumber=8762540' target='_blank'>" +
@@ -305,7 +305,7 @@ portfolioApp.controller('WorkCtrl', ['$rootScope', '$scope',
             {
                 id: "rgs",
                 href: "http://www8.hp.com/us/en/campaigns/workstations/remote-graphics-software.html",
-                title: "HP Remote Graphics<br/>Software",
+                title: "HP Remote Graphics<br>Software",
                 orientation: "wide",
                 hostenv: "Website",
                 indata: {xoff: 50, yoff: 0, zoom: 150},
@@ -323,7 +323,7 @@ portfolioApp.controller('WorkCtrl', ['$rootScope', '$scope',
             {
                 id: "graphicsdevice",
                 href: "",
-                title: "HPUX Graphics<br/>Device Drivers",
+                title: "HPUX Graphics<br>Device Drivers",
                 orientation: "wide",
                 hostenv: "",
                 indata: {xoff: 50, yoff: 50, zoom: 150},
@@ -341,7 +341,7 @@ portfolioApp.controller('WorkCtrl', ['$rootScope', '$scope',
             {
                 id: "x11",
                 href: "http://en.wikipedia.org/wiki/X_Window_System",
-                title: "X11 Windows<br/>System",
+                title: "X11 Windows<br>System",
                 orientation: "wide",
                 hostenv: "Website",
                 indata: {xoff: 50, yoff: 100, zoom: 150},
@@ -360,7 +360,7 @@ portfolioApp.controller('WorkCtrl', ['$rootScope', '$scope',
             "<p>John started work out of school as a Geophysical Engineer, working for SSC and Amerada Hess Research. " +
             "At SSC, he worked in the field as an Observer on a Seismic Crew shooting and gathering seismic data. He was " +
             "promoted to the Tulsa office to run their Seismic Data Processing lab. He was responsible for producing " +
-            "accurate seismic images of the subsurface for SSC's clients.</p><br/>" +
+            "accurate seismic images of the subsurface for SSC's clients.</p><br>" +
             "<p>John then hired on to work in the Amerada Hess Geophysics Research Lab in Tulsa, OK. " +
             "He worked with a team of scientists " +
             "experimenting on various image processing techniques to better image oil reserves in the " +
@@ -370,7 +370,7 @@ portfolioApp.controller('WorkCtrl', ['$rootScope', '$scope',
                 id: "seismicprocessing",
                 //href: "http://www.hess.com/",
                 href: "http://www.hess.com/technology/finding-oil-and-gas/quality-of-seismic",
-                title: "Seismic Data<br/>Processing<br/>@ Amerada Hess",
+                title: "Seismic Data<br>Processing<br>@ Amerada Hess",
                 orientation: "tall",
                 hostenv: "Website",
                 indata: {xoff: 10, yoff: 0, zoom: 150},
@@ -379,7 +379,7 @@ portfolioApp.controller('WorkCtrl', ['$rootScope', '$scope',
             {
                 id: "seismicacquisition",
                 href: "",
-                title: "Seismic Data<br/>Acquisition<br/>@ Seismograph<br/>Service<br/>Corporation",
+                title: "Seismic Data<br>Acquisition<br>@ Seismograph<br>Service<br>Corporation",
                 orientation: "wide",
                 hostenv: "",
                 indata: {xoff: 0, yoff: 0, zoom: 150},
